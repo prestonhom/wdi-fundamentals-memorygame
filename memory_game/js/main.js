3642f422d8d3ba8615bb78d181a636a5609a6226
Preston Hom <preston.hom@gmail.com>
2019-07-12 22:45:23 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,43 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank:"queen",
+		suit:"hearts",
+		cardImage:"images/queen-of-hearts.png"
+	},
+	{
+		rank:"queen",
+		suit:"diamonds",
+		cardImage:"images/queen-of-diamonds.png"
+	},
+	{
+		rank:"king",
+		suit:"hearts",
+		cardImage:"images/king-of-hearts.png"
+	},
+	{
+		rank:"king",
+		suit:"diamonds",
+		cardImage:"images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 };
 
 function flipCard(cardID){
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 }
 
 flipCard(0);
 flipCard(2);
 checkForMatch();
+
